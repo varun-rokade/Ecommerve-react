ceee966b84cb2095344af2ea385e67e465425ed5
Varun Rokade <rokadevarun9@gmail.com>
2023-01-17 22:52:35 +0530

    adding purchase page dynamic data

--- src/api/AppURL.js
+++ src/api/AppURL.js
@@ -2,6 +2,7 @@ class AppURL {
     static BaseUrl = "http://127.0.0.1:8000/api"
     static VisitorDetails = this.BaseUrl+"/getvisitor"
     static ContactDetails = this.BaseUrl+"/postcontact"
+    static AllSiteInfo = this.BaseUrl+"/allsiteinfo"
 }
 
 export default AppURL
--- src/components/common/PurchasePage.js
+++ src/components/common/PurchasePage.js
@@ -1,8 +1,35 @@
+import axios from "axios";
 import React, { Component } from "react";
 import { Fragment } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import AppURL from "../../api/AppURL";
+import ReactHtmlParser from 'react-html-parser';
 
 class PurchasePage extends Component {
+
+  constructor(){
+    super()
+    this.state={
+      purchase:""
+    }
+  }
+
+  componentDidMount(){
+    axios.get(AppURL.AllSiteInfo)
+    .then(response => {
+      let statusCode = response.status;
+      if(statusCode == 200){
+        let jsonData = (response.data)[0]['purchase_page']
+        this.setState({purchase:jsonData})
+
+      }
+    })
+    .catch(error => {
+
+    })
+  }
+
+
   render() {
     return (
       <Fragment>
@@ -11,28 +38,7 @@ class PurchasePage extends Component {
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12} >
               <h4 className="section-title-login">Purchase Page</h4>
               <p className="section-title-contact">
-                E-commerce is revolutionizing the way we all shop in India. Why
-                do you want to hop from one store to another in search of the
-                latest phone when you can find it on the Internet in a single
-                click? Not only mobiles. Flipkart houses everything you can
-                possibly imagine, from trending electronics like laptops,
-                tablets, smartphones, and mobile accessories to in-vogue fashion
-                staples like shoes, clothing and lifestyle accessories; from
-                modern furniture like sofa sets, dining tables, and wardrobes to
-                appliances that make your life easy like washing machines, TVs,
-                ACs, mixer grinder juicers and other time-saving kitchen and
-                small appliances; from home furnishings like cushion covers,
-                mattresses and bedsheets to toys and musical instruments, we got
-                them all covered. You name it, and you can stay assured about
-                finding them all here. For those of you with erratic working
-                hours, Flipkart is your best bet. Shop in your PJs, at night or
-                in the wee hours of the morning. This e-commerce never shuts
-                down. What's more, with our year-round shopping festivals and
-                events, our prices are irresistible. We're sure you'll find
-                yourself picking up more than what you had in mind. If you are
-                wondering why you should shop from Flipkart when there are
-                multiple options available to you, well, the below will answer
-                your question.
+                {ReactHtmlParser(this.state.purchase)}
               </p>
             </Col>
           </Row>
